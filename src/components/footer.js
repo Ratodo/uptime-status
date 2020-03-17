89ee5c57196e9c72697e35a582976941a4171664
Ratodo <34605543+Ratodo@users.noreply.github.com>
2020-03-18 00:45:42 +0800

    Update footer.js

--- src/components/footer.js
+++ src/components/footer.js
@@ -6,7 +6,7 @@ const Footer = () => {
     <div id="footer">
       <div className="container">
         <p>源码来自 <Link to="https://github.com/yb/uptime-status/" text="yb/uptime-status" /></p>
-        <p>基于 <Link to="https://uptimerobot.com/" text="UptimeRobot" /> 接口制作，检测频率 30 分钟</p>
+        <p>基于 <Link to="https://uptimerobot.com/" text="UptimeRobot" /> 接口制作，检测频率 5 分钟</p>
         <p>&copy; 2020 夜半观星 <Link to="https://t.ratodo.com/" text="T.RATODO.COM" /></p>
       </div>
     </div>
